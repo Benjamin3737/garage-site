552c552e2be3582d211b18e78d3a2e1c1eef70b9
Benjamin <benjamin.huynh.marais@gmail.com>
2026-07-03 04:07:37 +0200

    beaucoups de modifications d'un coup ajout prestation nettoyage correction prestation atelier petits ajustements un peu partout

--- src/Form/ChangePasswordFormType.php
+++ src/Form/ChangePasswordFormType.php
@@ -2,42 +2,30 @@
 
 namespace App\Form;
 
-use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\Email;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
-class RegistrationFormType extends AbstractType
+class ChangePasswordFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email', EmailType::class, [
-                'label' => 'Email',
-                'attr' => ['placeholder' => 'Votre email'],
-                'constraints' => [
-                    new NotBlank(message: 'Veuillez saisir un email.'),
-                    new Email(message: 'Veuillez saisir un email valide.'),
-                ],
-            ])
             ->add('plainPassword', RepeatedType::class, [
                 'type' => PasswordType::class,
                 'mapped' => false,
                 'required' => true,
-                'error_bubbling' => false,
                 'first_options' => [
-                    'label' => 'Mot de passe',
-                    'attr' => ['placeholder' => 'Mot de passe'],
+                    'label' => 'Nouveau mot de passe',
+                    'attr' => ['placeholder' => 'Nouveau mot de passe'],
                 ],
                 'second_options' => [
                     'label' => 'Confirmation',
-                    'attr' => ['placeholder' => 'Confirmez votre mot de passe'],
+                    'attr' => ['placeholder' => 'Confirmez votre nouveau mot de passe'],
                 ],
                 'invalid_message' => 'Les mots de passe doivent être identiques.',
                 'constraints' => [
@@ -53,8 +41,6 @@ class RegistrationFormType extends AbstractType
 
     public function configureOptions(OptionsResolver $resolver): void
     {
-        $resolver->setDefaults([
-            'data_class' => User::class,
-        ]);
+        $resolver->setDefaults([]);
     }
 }
